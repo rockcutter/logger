cd0e6d0a04a348edb031100d8bafabc21b8d8d5a
rockcutter <rockcutter414@gmail.com>
2023-04-03 00:05:21 +0900

    [change] google testを利用するように変更

--- LoggerTest.cpp
+++ LoggerTest.cpp
@@ -1,23 +1,5 @@
 #include "Logger.hpp"
+#include <sstream>
+#include <cassert>
+#include <gtest/gtest.h>
 
-int main(){
-    Logger::EnableAllLogger();
-    std::cout << "標準出力へのテスト" << std::endl;
-    auto logger = Logger::Get("myLogger");
-    logger->Debug("debug");
-    logger->Info("Info");
-    logger->Critical("Critical");
-    logger->Warning("Warning");
-    std::cout << "disable this logger" << std::endl;
-    logger->Disable();
-    logger->Debug("このメッセージは見えないはずだよ");
-    logger->Info("このメッセージは見えないはずだよ");
-    logger->Warning("このメッセージは見えないはずだよ");
-    logger->Critical("このメッセージは見えないはずだよ");
-    std::cout << "enable this logger" << std::endl;
-    logger->Enable();
-    logger->Debug   ("このメッセージは見えるはずだよ");
-    logger->Info    ("このメッセージは見えるはずだよ");
-    logger->Warning ("このメッセージは見えるはずだよ");
-    logger->Critical("このメッセージは見えるはずだよ");
-}
